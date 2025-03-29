0c421424f30966c2a1c1f31118dc4d3bf1accf12
keshariak <aryankeshari2003@gmail.com>
2025-03-29 16:26:52 +0530

    api done

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ const axios = require("axios");
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+// console.log(process.env.PORT)
+// console.log(process.env.MONGO_URI)
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+// OR allow specific origin
+app.use(cors({ origin: "https://pdf-qan-ui.vercel.app/" }));
 app.use(express.json());
 
 // Multer setup for file uploads
